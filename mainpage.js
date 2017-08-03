a93969482b263c9d39dc7606bfbb498bceedb5d8
Freshwinds <m13720362902@163.com>
2017-08-03 10:13:13 +0800

    -new page

---
+++ mainpage.js
@@ -0,0 +1,18 @@
+/**
+ * Created by freshwinds on 17-8-1.
+ */
+<div id="shine-girl" class="fl list">
+    <figure>
+        <img src="闪光少女.jepg" alt="二次元/古风/中西音乐" itemProp="image"/>
+         <figcaption style="display：none">
+             <h3 class="font-size14 font-family-yahei">
+                 <a href="1.html" title="二次元/古风/中西音乐" target="blank">
+                     二次元/古风/中西音乐</a>
+             </h3>
+             <span class="date" style="left:-200px">7,20,2017</span>
+             <a class="ds-button" href="1.html" target="blank">
+                 <span class="ds-button-inner">查看详情</span>
+             </a>
+         </figcaption>
+    </figure>
+</div>
