479568f9a5520b89ea77051116c578e774387fc6
Anchoret13 <vergil1133@live.com>
2017-08-04 09:56:15 +0800

    lalala

---
+++ 分类.js
@@ -0,0 +1,19 @@
+'use strict'
+/*
+$(document).read(function(){
+    let genreId;
+    $.get(`/types/${genreId}`,function(movieToType){
+        $("#movieId").value = movieToType.movieId;
+        $("#genreId").value = movieToType.genreId;
+    });
+});
+*/
+//首先 函数得到返回值，然后覆盖页面
+//
+$(document).ready(function(){
+    let genreId = 0;
+    $.get(`/types/${genreId}`,function(movie){
+        $("#movieName").html(movie.name);
+        $("#images").attr("src",`${movie.image}`);
+    })
+})
